fb7edbd7b267ed5110ec8551940cac502c877e6f
Michal Bernát <michal.bernat.l312@gmail.com>
2022-09-22 22:01:35 +0200

    using ConcatName() we format two strings into a single string

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            throw new NotImplementedException();
-        }
+            string name = ConcatName(lastName, firstName);
+            return name;
+        }    
     }
 }
