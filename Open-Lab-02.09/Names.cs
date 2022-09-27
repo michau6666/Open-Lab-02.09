30531a2bcb7d00e104df01b6aefb950bc67b9e61
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-09-27 09:47:14 +0200

    Update Names.cs
    returning full name with return

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -6,8 +6,7 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            string name = ConcatName(lastName, firstName);
-            return name;
+            return lastName + ", " + firstName;
         }    
     }
 }
